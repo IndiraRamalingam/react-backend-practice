846496f161e8992efa50834a25d13bf79d3866a2
IndiraRamalingam <indiraramalingam93@gmail.com>
2023-07-11 19:39:45 +0530

    Set the endpoints

--- index.js
+++ index.js
@@ -2,28 +2,40 @@
 
 //Simple web server
 
-const http =require('http');
-// let notes=[
-//     {
-//         id:1,
-//         content:"Content1",
-//         important:true
-//     },
-//     {
-//         id:2,
-//         content:"Content2",
-//         important:true
-//     },
-//     {
-//         id:3,
-//         content:"Content3",
-//         important:true
-//     }
-// ];
-const app=http.createServer((request,response)=>{
-    response.writeHead(200,{'Content-Type':'text/html'})
-    response.end("<h1>Hello world</h1>");
-});
+const express=require('express');
+const app= express();
+
+let notes=[
+    {
+        id:1,
+        content:"Content1",
+        important:true
+    },
+    {
+        id:2,
+        content:"Content2",
+        important:true
+    },
+    {
+        id:3,
+        content:"Content3",
+        important:true
+    }
+];
+
+//set the endpoints (set the / routes)
+// app.get('/',(request,response)=>{
+//     response.send("<h1>Hello world</h1>");
+// });
+
+//set the endpoints to get all the notes
+
+app.get('/api/notes',(request,response)=>{
+    response.json(notes);
+;})
+
 const PORT =3001;
-app.listen(PORT);
-console.log(`Server running on PORT ${PORT}`);
+app.listen(PORT,()=>{
+    console.log(`Server running on PORT ${PORT}`);
+});
+
