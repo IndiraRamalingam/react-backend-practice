7a0d9b2ad2d039858a305b3ff8ecc3b4eb8ff28a
IndiraRamalingam <indiraramalingam93@gmail.com>
2023-07-12 20:48:02 +0530

    backend mongodb + all notes

--- index.js
+++ index.js
@@ -4,103 +4,127 @@
 
 const express=require('express');
 const app= express();
+const mongoose = require('mongoose');
 
 //middleware
 app.use(express.json());
 
-let notes=[
-    {
-        id:1,
-        content:"Content1",
-        important:true
-    },
-    {
-        id:2,
-        content:"Content2",
-        important:true
-    },
-    {
-        id:3,
-        content:"Content3",
-        important:true
-    }
-];
+//console.log(process.argv);
 
-//set the endpoints (set the / routes)
-app.get('/',(request,response)=>{
-    response.send("<h1>Hello world</h1>");
+const url=`mongodb+srv://indiraramalingam93:arul8793@cluster0.5hvky8w.mongodb.net/NotesDB?retryWrites=true&w=majority`;
+
+mongoose.connect(url)
+    .then(() => {
+        console.log("connected to MongoDB")
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+
+//save a note in the db
+//define a schema
+
+const noteSchema = new mongoose.Schema({
+    content: String,
+    important: Boolean
 });
+
+//create a model
+const Note = mongoose.model('Note', noteSchema,'Notes');
+
+
+// let notes=[
+//     {
+//         id:1,
+//         content:"Content1",
+//         important:true
+//     },
+//     {
+//         id:2,
+//         content:"Content2",
+//         important:true
+//     },
+//     {
+//         id:3,
+//         content:"Content3",
+//         important:true
+//     }
+// ];
+
 
 //set the endpoints to get all the notes --> Read
 
 app.get('/api/notes',(request,response)=>{
-    response.json(notes);
+    Note.find({},{})
+        .then(notes =>{
+            response.status(200).json(notes);
+        })
 ;})
-
-//create a new resource based on the request data --> Create
-app.post('/api/notes',(request,response)=>{
-   notes=notes.concat(request.body);
-    // console.log(request.body);
-    response.status(201).json({message:'Successfully'});
-});
-
-//fetches a single resource based on id --> Read with params
-app.get('/api/notes/:id',(request,response) =>{
-    const id=request.params.id;
-    const note=notes.find(note=>note.id ==id);
-    if(note){
-        response.status(200).json(note);
-    }else{
-        //response.status(404).end('id does not exists)');
-        response.status(404).json({message:'id does not exists'});
-    }
-});
-
-//deletes a single  --> Delete 
-app.delete('/api/notes/:id',(request,response) =>{
-    //get the id
-    const id=request.params.id;
-    const note=notes.find(note => note.id ==id);
-     notes=notes.filter(note=>note.id !=id);
-    if(note){
-        response.status(204).json(note);
-    }else{
-        response.status(404).json({message:'id does not exists'});
-    }
-});
-
-//replaces the entire note object identified by id --> Update whole content
-
-app.put('/api/notes/:id',(request,response)=>{
-    //const id=Number(request.params.id);
-    const id=request.params.id;
-    const noteToReplace=request.body;
-    const note=notes.find(note => note.id ==id);
-    notes = notes.map(note => note.id==id ? noteToReplace :note);
-    
-    if(note){
-        response.status(200).json({message :'note replaced'});
-    }else{
-        response.status(404).json({message:'id does not exists'});
-    }
-})
-
-//replace the part of the object --> Updated with patch
-
-app.patch('/api/notes/:id',(request,response)=>{
-    //const id=Number(request.params.id);
-    const id=request.params.id;
-    const noteToReplace=request.body;
-
-    const note=notes.find(note => note.id ==id);
-    notes = notes.map(note => note.id==id ? {...note,...noteToReplace} :note);
- 
-    if(note){
-        response.status(200).json({message :'note patched'});
-    }else{
-        response.status(404).json({message:'id does not exists'});
-    }
-})
+// 
+// //create a new resource based on the request data --> Create
+// app.post('/api/notes',(request,response)=>{
+//    notes=notes.concat(request.body);
+//     // console.log(request.body);
+//     response.status(201).json({message:'Successfully'});
+// });
+// 
+// //fetches a single resource based on id --> Read with params
+// app.get('/api/notes/:id',(request,response) =>{
+//     const id=request.params.id;
+//     const note=notes.find(note=>note.id ==id);
+//     if(note){
+//         response.status(200).json(note);
+//     }else{
+//         //response.status(404).end('id does not exists)');
+//         response.status(404).json({message:'id does not exists'});
+//     }
+// });
+// 
+// //deletes a single  --> Delete 
+// app.delete('/api/notes/:id',(request,response) =>{
+//     //get the id
+//     const id=request.params.id;
+//     const note=notes.find(note => note.id ==id);
+//      notes=notes.filter(note=>note.id !=id);
+//     if(note){
+//         response.status(204).json(note);
+//     }else{
+//         response.status(404).json({message:'id does not exists'});
+//     }
+// });
+// 
+// //replaces the entire note object identified by id --> Update whole content
+// 
+// app.put('/api/notes/:id',(request,response)=>{
+//     //const id=Number(request.params.id);
+//     const id=request.params.id;
+//     const noteToReplace=request.body;
+//     const note=notes.find(note => note.id ==id);
+//     notes = notes.map(note => note.id==id ? noteToReplace :note);
+//     
+//     if(note){
+//         response.status(200).json({message :'note replaced'});
+//     }else{
+//         response.status(404).json({message:'id does not exists'});
+//     }
+// })
+// 
+// //replace the part of the object --> Updated with patch
+// 
+// app.patch('/api/notes/:id',(request,response)=>{
+//     //const id=Number(request.params.id);
+//     const id=request.params.id;
+//     const noteToReplace=request.body;
+// 
+//     const note=notes.find(note => note.id ==id);
+//     notes = notes.map(note => note.id==id ? {...note,...noteToReplace} :note);
+//  
+//     if(note){
+//         response.status(200).json({message :'note patched'});
+//     }else{
+//         response.status(404).json({message:'id does not exists'});
+//     }
+// })
 
 
 const PORT =3001;
---
+++ mongo.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose');
+//console.log(process.argv);
+
+const url=`mongodb+srv://indiraramalingam93:arul8793@cluster0.5hvky8w.mongodb.net/NotesDB?retryWrites=true&w=majority`;
+
+mongoose.connect(url)
+    .then(() => {
+        console.log("connected to MongoDB")
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+
+//save a note in the db
+//define a schema
+
+const noteSchema = new mongoose.Schema({
+    content: String,
+    important: Boolean
+});
+
+//create a model
+const Note = mongoose.model('Note', noteSchema,'Notes');
+
+// //actual data to store in the database
+// const note= new Note({
+//     content:'mongodb2',
+//     important:true,
+// });
+// 
+// //save a note in the db
+// note.save()
+//     .then((result) =>{
+//         console.log("note saved");
+//         mongoose.connection.close();
+//     });
+
+// //For multiple data
+// let notes=[
+//     {
+//         content:'data1',
+//         important:true,
+//     },
+//     {
+//         content:'data2',
+//         important:true,
+//     }
+// ];
+// 
+// notes.forEach((note) =>{
+//     let noteModel=new Note(note);
+// 
+//     noteModel.save()
+//     .then((result) =>{
+//         console.log("note saved");
+//         mongoose.connection.close();
+//     });
+// 
+// 
+// })
+
+Note.find({}, {})
+    .then(notes =>{
+        console.log(notes);
+        mongoose.connection.close();
+    });
